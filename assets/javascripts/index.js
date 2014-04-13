1c3115e744cc4588e41b45cce7b87ab62d020853
Stew Smith <stewartlsmith12@gmail.com>
2014-04-13 01:00:42 -0400

    acceleramoeter threshold

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,4 +1,12 @@
+var prev_accel;
+var prev_x = 0;
+var prev_y = 0;
+var prev_z = 0;
+
 window.ondevicemotion = function (e) {
   var accel = e.accelerationIncludingGravity;
-  document.body.innerHTML = accel.x + "|" + accel.x + "|" + accel.x + "|"
+  if (Math.abs(accel.x - prev_x) > 5) {
+      document.body.innerHTML = accel.x + "|" + accel.y + "|" + accel.z + "|"
+  }
+  prev_accel = accel
 }
