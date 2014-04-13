d98889b3a1846fb8bbcbb03a370d3a4100e9d18b
Ian Lozinski <ian.lozinski@gmail.com>
2014-04-13 02:25:42 +0000

    added rooms and bullshit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var app = express();
-var io = require('socket.io').listen(app);
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,46 +19,65 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function (req, res){
-    res.sendfile( __dirname +    
+app.get('/', function (req, res) {
+  res.sendfile( __dirname + '/views/index.html')
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(e) {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-
-function popCheck(roomId){
-    var pop = io.sockets.clients(roomId).length;
-    if(pop == 2){
-        console.log("returning a 1");
-        return 1;
-    }else{
-        console.log("new player joining room" + roomId);
-        return 0;
-    }
+function popCheck(roomId) {
+  var pop = io.sockets.clients(roomId).length;
+  if (pop == 2) {
+    console.log("returning a 1");
+    return 1;
+  } else {
+    console.log("new player joining room" + roomId);
+    return 0;
+  }
 }
 
-var activeRooms = {};
+/*
+ * casts to clients
+ */
+function flipGameState(socket) {
+  socket.broadcast.emit('toggleGameState');
+}
 
-io.sockets.on('connection' function(socket){
-	console.log('connection detected');
+var activeRooms = new Object();
 
-	socket.on('newJoin', function(roomId){
-    	if(activeRooms.indexOf(roomId) == -1){
-            //new room
-            socket.join(roomId);
-            activeRooms.push(roomId);
-            socket.emit('roomStatus',1);
-            console.log("the value of roomID: "+ roomId + "the value of disconnectId: "+ disconnectId);
-        }else if(popCheck(roomId) ==0){
-            //joining a room with one other person
-            socket.join(roomId);
-            socket.emit('roomStatus', 2);
-            console.log("the value of roomID: "+ roomId + "the value of disconnectId: "+ disconnectId);
-        }else{
-            //room is full
-            socket.emit('roomStatus', 3);
-    	}
-    })
+io.sockets.on('connection', function(socket) {
+  var roomNumber;
+  var timer;
+  console.log('connection detected');
+
+  socket.on('newJoin', function(roomId) {
+    if (activeRooms.indexOf(roomId) == -1) {
+      //new room
+      socket.join(roomId);
+      activeRooms.push(roomId);
+      roomNumber = roomId;
+      socket.emit('roomStatus', 1);
+      console.log("the value of roomID: "+ roomId + "the value of disconnectId: "+ disconnectId);
+    } else if (popCheck(roomId) == 0) {
+      //joining a room with one other person
+      socket.join(roomId);
+      roomNumber = roomId;
+      socket.emit('roomStatus', 2);
+      console.log("the value of roomID: " + roomId)
+    } else {
+      //room is full
+      socket.emit('roomStatus', 3);
+    }
+  })
+
+  socket.on('gamestart', function() {
+    socket.broadcast.emit('gamestart', 'msg');
+    setInterval(function() {
+      flipGameState(socket)
+    }, 5000);
+  })
+
 })
+
