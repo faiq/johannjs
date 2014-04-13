0ea22261d4438d40c102a84fef768c62d441f3f7
fr151 <faiqrazarizvi@gmail.com>
2014-04-12 20:43:36 -0400

    starting test

---
+++ assets/javascripts/index.js
@@ -0,0 +1,5 @@
+window.ondevicemotion = function (e) {
+    conosle.log(e.accelerationIncludingGravity.x); 
+    consele.log(e.accelerationIncludingGravity.y); 
+    console.log(e.accelerationIncludingGravity.z);
+} 
